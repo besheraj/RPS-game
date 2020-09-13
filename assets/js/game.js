4ed38edb08cc306a6a7026c7f1b2913523245bcb
Behser Aj <besher.aj@outlook.com>
2020-09-13 12:58:18 +0800

    copied this project from the milestone 2

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,63 +1,63 @@
+// added the main variables for the game //
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
+
+// added the scores that will be displayed on the main page//
 const displaycomputerScore = document.getElementById("cscore");
 const displayhumanScore = document.getElementById("uscore");
 const displaycurrentRoundNumber = document.getElementById("rounds");
 const displaycomputerchoice = document.getElementById("computer-choice");
 
-
-
-
+// user choices of input //
 const getUserChoice = userInput => {
     userInput = userInput.toLowerCase();
     if (userInput === 'rock' ||
         userInput === 'paper' ||
         userInput === 'scissors') { return userInput; } else { console.log('Error!') }
 };
+// generating computer choice //
 const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
-        case 0: return 'Rock'
-        case 1: return 'Paper'
-        case 2: return 'Scissors'
+        case 0: return 'rock'
+        case 1: return 'paper'
+        case 2: return 'scissors'
     }
 }
-
+// decide the winner //
 const determineWinner = (userChoice, computerChoice) => {
-    if (userChoice === computerChoice) { return 'The Game is a tie' }
+    if (userChoice === computerChoice) {
+        return 'The Game is a tie'
+    }
 
     if (userChoice === 'rock') {
-        if (computerChoice === 'Paper') {
+        if (computerChoice === 'paper') {
             return 'The Computer Won!';
         } else {
             return 'You Won!';
         }
     }
     if (userChoice === 'paper') {
-        if (computerChoice === 'Scissors') {
+        if (computerChoice === 'scissors') {
             return 'The Computer Won!';
         } else {
             return 'You Won!'
         }
     }
     if (userChoice === 'scissors') {
-        if (computerChoice === 'Rock') {
+        if (computerChoice === 'rock') {
             return 'The Computer Won!';
         } else {
             return 'You Won!'
         }
     }
 };
-
-
-
+//when game played selecting user choice then generate computer choice determine 
+// the winner update the score for winner and advance by one round //
 const playGame = () => {
-    const userChoice = document.querySelector('input[name="choices"]:checked').value;
-    // const userChoice = document.getElementById("choices").value;
-
+        const userChoice = document.querySelector('input[name="choices"]:checked').value;
     const computerChoice = getComputerChoice();
-
     const winner = determineWinner(userChoice, computerChoice);
 
     const updateScore = () => {
@@ -71,19 +71,10 @@ const playGame = () => {
 
     document.getElementById("result").innerHTML = '<div class="summary"> ' + determineWinner(userChoice, computerChoice) + '</div>';
     updateScore();
-    
+
     displaycomputerchoice.innerText = computerChoice;
     displaycomputerScore.innerText = computerScore;
     displayhumanScore.innerText = humanScore;
     displaycurrentRoundNumber.innerText = currentRoundNumber;
     advanceRound();
-
-
 };
-
-
-
-
-
-
-
